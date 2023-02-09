208d31ac9528d1f6c916f3398a9c021078d76c8a
Daniela Marques <116357067+daniela2785@users.noreply.github.com>
2023-02-09 17:58:34 -0300

    Update Program.cs

--- Matriz01/Program.cs
+++ Matriz01/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+
+
+namespace Matriz01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //03
+            int N = int.Parse(Console.ReadLine());
+
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            int[] mat1 = new int[A];
+            int[] mat2 = new int[B];
+
+            for (int i = 0; i < A; i++)
+            {
+                valores = Console.ReadLine().Split(' ');
+                for (int j = 0; j < N; j++)
+                {
+                    mat1[i] = int.Parse(valores[i]);
+                    mat2[2] = int.Parse(valores[j]);
+                }
+            }
+
+            Console.WriteLine("soma");
+            for (int i = 0; i < A; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    int soma = mat1[i] + mat2[j];
+                }
+            }
+        }
+    }
+}
